ea9725b97a05e9437cdc6c01651840908a2bfbfa
Faysel Abdella <fayselcode@gmail.com>
2025-07-21 12:18:46 +0300

    change the status input to radio

--- src/app/admin/(adminWrapper)/add-property/_components/BasicInfo.tsx
+++ src/app/admin/(adminWrapper)/add-property/_components/BasicInfo.tsx
@@ -1,5 +1,7 @@
 import TextInput from "@/components/block/FormInput";
 import FormWrapper from "@/components/block/FormWrapper";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Home } from "lucide-react";
 import React from "react";
 
@@ -28,17 +30,48 @@ const BasicInfo = () => {
           <TextInput
             label="Price Amount"
             isRequired
-            className="flex-1"
+            className="flex-1 w-1/2"
             placeholder="Enter price"
             type="text"
           />
-          <TextInput
-            label="Status"
-            className=" flex-1"
-            isRequired
-            placeholder="Available"
-            type="text"
-          />
+          <div className="w-1/3 flex-col items-center">
+            <Label className="text-[#333C4C] font-medium text-[14px]">
+              Status*
+            </Label>
+            <div className="flex items-center gap-4 mt-3">
+              <div className="flex items-center gap-2">
+                <Input
+                  id="available"
+                  type="radio"
+                  name="status"
+                  value="available"
+                  defaultChecked
+                  className="size-5"
+                />
+                <Label htmlFor="available">Available</Label>
+              </div>
+              <div className="flex items-center gap-2">
+                <Input
+                  id="pending"
+                  type="radio"
+                  name="status"
+                  value="pending"
+                  className="size-5"
+                />
+                <Label htmlFor="pending">Pending</Label>
+              </div>
+              <div className="flex items-center gap-2">
+                <Input
+                  id="sold"
+                  type="radio"
+                  name="status"
+                  value="sold"
+                  className="size-5"
+                />
+                <Label htmlFor="sold">Sold</Label>
+              </div>
+            </div>
+          </div>
         </div>
         <TextInput
           label="Full Address"
