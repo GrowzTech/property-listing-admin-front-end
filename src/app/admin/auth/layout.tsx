81651cc4b4ccfc78c653da0e8a1a4bec2f14f5e2
Faysel Abdella <fayselcode@gmail.com>
2025-07-24 17:50:36 +0300

    up

--- src/app/admin/auth/layout.tsx
+++ src/app/admin/auth/layout.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { redirect } from "next/navigation";
 import AuthRoutes from "@/app/AuthRoutes";
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
+  redirect("/admin/dashboard");
   return (
     <AuthRoutes>
       <div className="font-literata">{children}</div>
