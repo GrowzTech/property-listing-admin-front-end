f334708b605834461001f7475f16d0c6f406fa05
Faysel Abdella <fayselcode@gmail.com>
2025-07-24 17:41:51 +0300

    up

--- src/app/admin/(adminWrapper)/layout.tsx
+++ src/app/admin/(adminWrapper)/layout.tsx
@@ -8,17 +8,17 @@ import { AppSidebar } from "@/components/block/Sidebar";
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <ProtectedRoute>
-    <SidebarProvider>
-      <AppSidebar />
-      <div className=" w-screen h-screen text-black bg-[#F9FAFB] relative">
-        <div className="absolute py-4 px-3 flex justify-center items-center z-50 ">
-          <SidebarTrigger color="#374151" />
+      <SidebarProvider>
+        <AppSidebar />
+        <div className=" w-screen h-screen text-black bg-[#F9FAFB] relative">
+          <div className="absolute py-4 px-3 flex justify-center items-center z-50 ">
+            <SidebarTrigger color="#374151" />
+          </div>
+          <div className="w-full h-screen bg-[#F9FAFB] text-black font-inter">
+            {children}
+          </div>
         </div>
-        <div className="w-full h-screen bg-[#F9FAFB] text-black font-inter">
-          {children}
-        </div>
-      </div>
-    </SidebarProvider>
-     </ProtectedRoute>
+      </SidebarProvider>
+    </ProtectedRoute>
   );
 }
